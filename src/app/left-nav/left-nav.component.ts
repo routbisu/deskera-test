fefb64f3d58a5aae6aca63779588c34f9c5c8d7c
Biswaranjan Rout <bisu9861861136@gmail.com>
2019-04-07 16:19:21 +0530

    Changing " to ' for tslint

--- src/app/left-nav/left-nav.component.ts
+++ src/app/left-nav/left-nav.component.ts
@@ -1,9 +1,9 @@
-import { Component } from "@angular/core";
+import { Component } from '@angular/core';
 // import
 
 @Component({
-  selector: "left-nav",
-  template: require("./left-nav.component.html"),
-  styles: [require("./left-nav.component.scss")]
+  selector: 'left-nav',
+  template: require('./left-nav.component.html'),
+  styles: [require('./left-nav.component.scss')]
 })
 export class LeftNavComponent {}
