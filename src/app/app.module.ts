da3c14f30ab111fcfafb58522d68b91ce91d045a
Biswaranjan Rout <biswaranjan.rout@wdc.com>
2019-04-07 16:33:41 +0530

    Working on chat

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,15 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { AppComponent } from './app.component';
-import { LeftNavComponent } from './left-nav/left-nav.component';
-import { MainContentComponent } from './main-content/main-content.component';
-import { ChatHeaderComponent } from './main-content/chat-header/chat-header.component';
-import { ChatWindowComponent } from './main-content/chat-window/chat-window.component';
-import { UpdateProfileModalComponent } from './main-content/update-profile-modal/update-profile-modal.component';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+
+import { WebsocketService } from "./services/socket/socket.service";
+import { ChatService } from "./services/chat/chat.service";
+
+import { AppComponent } from "./app.component";
+import { LeftNavComponent } from "./left-nav/left-nav.component";
+import { MainContentComponent } from "./main-content/main-content.component";
+import { ChatHeaderComponent } from "./main-content/chat-header/chat-header.component";
+import { ChatWindowComponent } from "./main-content/chat-window/chat-window.component";
+import { UpdateProfileModalComponent } from "./main-content/update-profile-modal/update-profile-modal.component";
 
 @NgModule({
   imports: [BrowserModule],
@@ -17,6 +21,7 @@ import { UpdateProfileModalComponent } from './main-content/update-profile-modal
     ChatHeaderComponent,
     ChatWindowComponent,
     UpdateProfileModalComponent
-  ]
+  ],
+  providers: [ChatService, WebsocketService]
 })
 export class AppModule {}
--- src/app/main-content/chat-window/chat-window.component.ts
+++ src/app/main-content/chat-window/chat-window.component.ts
@@ -1,8 +1,32 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { ChatService } from "../../services/chat/chat.service";
 
 @Component({
-  selector: 'chat-window',
-  template: require('./chat-window.component.html'),
-  styles: [require('./chat-window.component.scss')]
+  selector: "chat-window",
+  template: require("./chat-window.component.html"),
+  styles: [require("./chat-window.component.scss")]
 })
-export class ChatWindowComponent {}
+export class ChatWindowComponent {
+  constructor(private chat: ChatService) {}
+
+  messages: any[] = [];
+
+  ngOnInit() {
+    this.chat.messages.subscribe(msg => {
+      console.log("message", msg);
+      if (msg && msg.text) {
+        const text = JSON.parse(msg.text);
+        this.messages.push(text.message);
+      }
+    });
+  }
+
+  sendMessage(message) {
+    const obj = {
+      to: "routbisu",
+      from: "ashunayak",
+      message: "Hello world"
+    };
+    this.chat.sendMsg(obj);
+  }
+}
